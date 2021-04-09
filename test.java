4f26a499163bedbc926d759d78cc48a101d0e9db
lhb-123-lang <69503865+lhb-123-lang@users.noreply.github.com>
2021-04-09 17:32:17 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        //stack.add(3);
+        stack.push(3);
+        System.out.println(stack.pop());
+
+    }
+
+}
